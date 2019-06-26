b2c773b97a4b115788607f7265056278118eaad4
JSHEN9085 <jshen9085@gmail.com>
2019-06-25 22:37:36 -0400

    new controller in process

---
+++ src/main/java/com/jshen/crudjpa/rest/EmployeeRestController.java
@@ -0,0 +1,55 @@
+package com.jshen.crudjpa.rest;
+
+import com.jshen.crudjpa.entity.Employee;
+import com.jshen.crudjpa.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee findById(int id){
+        Employee employee = employeeService.findById(id);
+
+        if(employee == null){
+            throw new RuntimeException("Did not find employee with id - " + id);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+        employee.setId(0);
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees/{id}")
+    public Employee UpdateEmployee (@RequestBody Employee employee){
+        //just for testing, did not add the logic that check if the employee exist;
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @DeleteMapping("employees/{id}")
+    public String 
+
+
+}
