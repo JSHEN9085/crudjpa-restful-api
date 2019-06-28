7b6759c017162eb07df07016a040862d54fc423f
JSHEN9085 <jshen9085@gmail.com>
2019-06-27 20:57:51 -0400

    spring-boot data rest

--- src/main/java/com/jshen/crudjpa/CrudjpaApplication.java
+++ src/main/java/com/jshen/crudjpa/CrudjpaApplication.java
@@ -2,18 +2,9 @@ package com.jshen.crudjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-
 
 @SpringBootApplication
-@ComponentScan("com")
-public class CrudjpaApplication extends SpringBootServletInitializer {
-
-	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(CrudjpaApplication.class);
-	}
+public class CrudjpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudjpaApplication.class, args);
